781bf1228e05fc172f61f7a5cf1156a81322fca1
cindelint <cindylee776@gmail.com>
2019-02-13 08:39:53 -0800

    started thing

--- build/source/FractalTree.java
+++ build/source/FractalTree.java
@@ -1,9 +1,25 @@
-private double fractionLength = .8;
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class FractalTree extends PApplet {
+
+private double fractionLength = .8f;
 private int smallestBranch = 10;
-private double branchAngle = .2;
+private double branchAngle = .2f;
 public void setup()
 {
-	size(640,480);
+	
 	noLoop();
 }
 public void draw()
@@ -30,4 +46,14 @@ public void drawBranches(int x,int y, double branchLength, double angle)
 		drawBranches(finX1, finY1, newLength, newAng);
 		drawBranches(finX2, finY2, newLength, newAng);
 	}
+}
+  public void settings() { 	size(640,480); }
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "FractalTree" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
